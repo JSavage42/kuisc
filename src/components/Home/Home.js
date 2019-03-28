28053ddb87ac867c4407ee9cde8034dade3a3165
Jason Savage <jasavage42@gmail.com>
2019-03-27 19:21:38 -0500

    Updated images and nav hover

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,32 +1,29 @@
-import React from "react";
-import jayhawkImg from '../../assets/images/jayHawk.png';
-import "./Home.css";
+import React from 'react';
+import jayhackerSeal from '../../assets/images/jayhackersSeal.png';
+import './Home.css';
 
 const Home = () => (
   <main className="home">
     <section>
       <header className="hero">
         <h2>KU Information Security Club</h2>
-        <img src={jayhawkImg} alt="Jayhawk" />
+        <img src={jayhackerSeal} alt="Jayhawk" />
       </header>
       <h3>Speakers From the Industry</h3>
       <p>
-        We bring in a variety of speakers from industry to talk about the work
-        they are involved in and to provide some insight about pursuing cyber
-        security as a profession.
+        We bring in a variety of speakers from industry to talk about the work they are involved in and to provide some
+        insight about pursuing cyber security as a profession.
       </p>
       <h3>Competitions</h3>
       <p>
-        As a club, we’ve attended several cyber defense competitions ranging
-        from the Central Area Networking and Security workshop (CANSec), to the
-        regional qualifiers for the national collegiate cyber defense
-        competition. We’ve also hosted a few competitions ourselves.
+        As a club, we’ve attended several cyber defense competitions ranging from the Central Area Networking and
+        Security workshop (CANSec), to the regional qualifiers for the national collegiate cyber defense competition.
+        We’ve also hosted a few competitions ourselves.
       </p>
       <h3>Hacking Labs</h3>
       <p>
-        Gain hands on experience with various tools included in Kali Linux,
-        learn about proper configuration of systems and services, and how to use
-        various networking tools{" "}
+        Gain hands on experience with various tools included in Kali Linux, learn about proper configuration of systems
+        and services, and how to use various networking tools{' '}
       </p>
     </section>
   </main>
